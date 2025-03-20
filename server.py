bad15ff40d4a8428fa94de19109b038f023805d8
MurtazaKush <murtazakushalgadhwala@gmail.com>
2025-03-20 15:11:45 +0530

    change password complete

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
-from sqlmodel import SQLModel, Field, create_engine, Session, select
+from sqlmodel import SQLModel, Field, create_engine, Session, select, or_
 import os
 from pydantic import BaseModel
 from enum import Enum
@@ -55,13 +55,16 @@ class Requests(SQLModel,table=True):
 class User_F(BaseModel):
     username: str
     passhash: str
-
-class User_CF(BaseModel):
+class secret_list(BaseModel):
+    owner_secret: list[owner_doc]
+    people_secret: list[people_doc]
+    valid: bool
+class User_CP(BaseModel):
     username: str
     oldpasshash: str
     newpasshash: str
-    updated_secret: list
-    newpb:str
+    updated_secret: secret_list
+    newpb: str
 
 # Create Tables if They Don't Exist
 def create_db_and_tables():
@@ -87,7 +90,7 @@ def on_startup():
 # Route to Create a New User
 @app.post("/signup/", response_model=User)
 def create_user(user: User, db: Session = Depends(get_db)):
-    ruser=db.exec(select(User).where(User.username==user.username)).first()
+    ruser=db.exec(select(User).where(User.username==user.username)).one()
     if ruser:
         user.username="!"+user.username
         return user
@@ -99,24 +102,48 @@ def create_user(user: User, db: Session = Depends(get_db)):
 # Login route for user
 @app.post("/login/", response_model=bool)
 def login_users(user:User_F,db: Session = Depends(get_db)):
-    ruser=db.exec(select(User).where(User.username==user.username)).first()
+    ruser=db.exec(select(User).where(User.username==user.username)).one()
     if not ruser:
         return False
     if ruser.passhash!=user.passhash:
         return False
     return True
 
-@app.get("/change_pass/",response_model=list)
+@app.get("/change_pass/",response_model=secret_list)
 def change_password_get(user:User_F,db: Session = Depends(get_db)):
-    ruser=db.exec(select(User).where(User.username==user.username)).first()
+    ruser=db.exec(select(User).where(User.username==user.username)).one()
+    l=secret_list()
+    l.owner_secret=[]
+    l.people_secret=[]
+    l.valid=False
     if not ruser:
-        return [False]
+        return l
     if ruser.passhash!=user.passhash:
-        return [False]
-    l=db.exec(select(people_doc).where(people_doc.user_id==user.username)).all()
-    l+=db.exec(select(owner_doc).where(owner_doc.owner_id==user.username)).all()
+        return l
+    l.owner_secret=db.exec(select(people_doc).where(people_doc.user_id==user.username)).all()
+    l.people_secret=db.exec(select(owner_doc).where(owner_doc.owner_id==user.username)).all()
+    l.valid=True
     return l
 
 @app.post("/change_pass/",response_model=bool)
-def change_password_post(user:User_F,db: Session = Depends(get_db)):
-    pass
+def change_password_post(user:User_CP,db: Session = Depends(get_db)):
+    u=User_F()
+    u.username=user.username
+    u.passhash=user.oldpasshash
+    if login_users(u,db)==False:
+        return False
+    user_record=db.exec(select(User).where(User.username == user.username)).one()
+    user_record.passhash=user.newpasshash
+    user_record.pb_key=user.newpb
+    db.add(user_record)
+    for o_s in user.updated_secret.owner_secret:
+        o_s_record=db.exec(select(owner_doc).where(owner_doc.doc_id==o_s.doc_id,owner_doc.owner_id==o_s.owner_id)).one()
+        o_s_record.encrypted_secret=o_s.encrypted_secret
+        db.add(o_s_record)
+    for p_s in user.updated_secret.people_secret:
+        p_s_record=db.exec(select(people_doc).where(people_doc.doc_id==p_s.doc_id,people_doc.user_id==p_s.user_id)).one()
+        p_s_record.encrypted_secret=p_s.encrypted_secret
+        db.add(p_s_record)
+    db.commit()
+    return True
+
