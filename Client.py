d9589251588e554f463f9a7c617a84dfa239f073
MurtazaKush <murtazakushalgadhwala@gmail.com>
2025-03-25 15:50:05 +0530

    client fix

--- Client.py
+++ Client.py
@@ -7,10 +7,10 @@ from rich.console import Console
 from rich.table import Table
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-from hash_rsa import generate_hash, hash_to_rsa_key
-from Doc_encryption import encrypt_doc
-from SSS import spilt_secret, get_secret
-from Doc_Decryption import decrypt_doc, bitstring_to_bytes
+from .hash_rsa import generate_hash, hash_to_rsa_key
+from .Doc_encryption import encrypt_doc
+from .SSS import spilt_secret, get_secret
+from .Doc_Decryption import decrypt_doc, bitstring_to_bytes
 
 BASE_URL = "http://127.0.0.1:8000"
 console = Console()
